cfa58caf1a12d810d9211fa9ccc258661569a155
Yiying Jie <yiying.j@outlook.com>
2021-04-12 16:44:16 -0700

    initial working app with all features implemented except for copy

---
+++ app.js
@@ -0,0 +1,37 @@
+const vm = Vue.createApp({
+    data() {
+        return {
+            perspective: 200,
+            rotateX: 0,
+            rotateY: 0,
+            rotateZ: 0
+        }
+    }, 
+    methods: {
+        resetBox() {
+            this.perspective = 200;
+            this.rotateX = 0;
+            this.rotateY = 0;
+            this.rotateZ = 0;
+        },
+        copyVal() {
+            let transformVal = `transform: 
+                                perspective: ${this.perspective}
+                                rotateX: ${this.rotateX}
+                                rotateY: ${this.rotateY}
+                                rotateZ: ${this.rotateZ}`
+            
+            document.execCommand("copy");
+        }
+    },
+
+    computed: {
+        transformBox() {
+            return { transform: `perspective(${this.perspective}px) 
+            rotateX(${this.rotateX}deg) 
+            rotateY(${this.rotateY}deg) 
+            rotateZ(${this.rotateZ}deg)` }
+        },
+    }
+
+}).mount('#app');
